1b85e13e2e15d9e826bc02f7e75a03046c1da5a0
Vithushan Nama <vithushan@Vithushans-MacBook-Pro.local>
2016-08-17 17:15:50 +0200

    add CORS support

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -11,6 +12,8 @@ var routes = require('./server/routes/index');
 var api = require('./server/routes/api');
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'server/views'));
